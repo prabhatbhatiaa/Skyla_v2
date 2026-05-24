346b9b6a650b3e132ab812d3549b2c541307feea
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-05-24 14:55:01 +0530

    Adjusted old v12 event handler to v14. Functioning

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+    const eventFiles = fs
+        .readdirSync('./src/events')
+        .filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        const event =
+            require(`../events/${file}`);
+
+        if (event.once) {
+            client.once(event.name, (...args) =>
+                event.execute(...args)
+            );
+        } else {
+            client.on(event.name, (...args) =>
+                event.execute(...args)
+            );
+        }
+        console.log(`Loaded event: ${event.name}`);
+    }
+};
