1c8a5edfee92b562ed146041ce8dd10db82067ef
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-05-24 14:34:26 +0530

    Rebuilt deploy command style to analyse folders

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,26 +1,42 @@
 // Upload slash command to Discord API
 
 require('dotenv').config();
+const fs = require('fs');
 const { REST, Routes } = require('discord.js');
-const pingCommand = require('./commands/info/ping');
 
-const commands = [pingCommand.data.toJSON()];
+const commands = [];
+const commandFolders =
+    fs.readdirSync('./src/commands');
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+for (const folder of commandFolders) {
+    const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command =
+            require(`./commands/${folder}/${file}`);
+
+        commands.push(command.data.toJSON());
+    }
+}
+
+const rest = new REST({ version: '10' })
+    .setToken(process.env.TOKEN);
 
 (async () => {
-    try{
-        console.log("Register Slash Commands..")
-        await rest.put (
+    try {
+        console.log('Registering slash commands...');
+        await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID,
                 process.env.GUILD_ID
             ),
             { body: commands }
         );
-        console.log("Slash commands uploadeded!")
-    }
-    catch(err) {
-        console.error(err);
+        console.log('Slash commands registered.');
+    } catch (error) {
+        console.error(error);
+
     }
 })();
