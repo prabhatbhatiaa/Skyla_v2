88b015166e921ab548735e73680eeeac05ac1908
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-05-24 14:57:10 +0530

    Cleaned the index.js. Handler implemnted Bot

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 // Main config of Bot 
 
 require('dotenv').config();
-// const fs = require('fs');
 const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
 
 const commandHandler = require('./handlers/commandHandler');
+const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
 commandHandler(client);
-
-// const commandFiles = fs
-//     .readdirSync('./src/commands')
-//     .filter(file => file.endsWith('.js'));
-
-// for (const file of commandFiles) {
-//     const command = require(`./commands/${file}`);
-//     client.commands.set(command.data.name, command);
-// }
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-        await command.execute(interaction);
-    } 
-    catch (error) {
-        console.error(error);
-        await interaction.reply({
-            content: 'Error executing command.',
-            ephemeral: true
-        });
-    }
-});
+eventHandler(client);
 
 client.login(process.env.TOKEN);
