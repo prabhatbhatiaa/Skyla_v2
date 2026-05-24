3ed71f1632450e05406980ca64d96736e6f63b96
prabhatbhatiaa <prabhatbhatia09@gmail.com>
2026-05-24 15:09:12 +0530

    Added an express server to expose url for hosting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Main config of Bot 
 
+const express = require('express');
 require('dotenv').config();
 const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
 
@@ -12,5 +13,14 @@ const client = new Client({
 
 commandHandler(client);
 eventHandler(client);
+
+const app = express();
+app.get('/', (req,res) => {
+    res.send('Skyla is online!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 client.login(process.env.TOKEN);
